1967a6fba78cec8476c39a18e377dc6e47a1b8d1
Eoin-Mur <eoin.murphy175@gmail.com>
2015-10-19 04:15:57 +0100

    Inital Database Creation
    Script to create the database, table to store transfers dataset and
    import the scraped csv file to a table (see comm's in file) which is to
    be used by a procedure to clean up the inital scraped files (clean text
    column to extract cost, Loan, Back Loan. Also to fix the missing owner
    values using the id's added during the scrape), should have that done
    tomorrow morning

---
+++ SQL-Files/Inital_Database_Creation.sql
@@ -0,0 +1,58 @@
+CREATE SCHEMA IF NOT EXISTS data_mining_assignment;
+
+USE data_mining_assignment;
+
+CREATE TABLE IF NOT EXISTS TransferHistory
+(
+	TransferId INT NOT NULL AUTO_INCREMENT,
+	Team VARCHAR(35) NOT NULL,
+    League varchar(60) NOT NULL,
+    LeagueId int NOT NULL,
+    Year YEAR NOT NULL,
+    TransferType VARCHAR(10),
+    PlayerSurname VARCHAR(55),
+    Cost REAL,
+    Loan INT NOT NULL,
+    BackLoan INT NOT NULL,
+    OTeam VARCHAR(100),
+    TTeam VARCHAR(100),    
+    primary key(TransferId)
+);
+
+#Most pain in the arse function ever!!!
+#first doenst allow file_name as a paramater 
+#and then is not allowed to be in stored procedures!!!! WHY!
+
+#work around:
+#just create what was supposed to be a temp table in a procedure as a proper one
+#import it and then just do all the clean up from a procedure working of this table
+
+CREATE TABLE IF NOT EXISTS TransfersImport
+(
+	Id int NOT NULL AUTO_INCREMENT,
+	TransferType varchar(10),
+    Text varchar(255),
+    Year year,
+    Season int,
+    Player varchar(255),
+    RowId int,
+    Team varchar(255),
+    Owner varchar(255),
+    TransferTypeId varchar(255),
+    PRIMARY KEY(Id)
+);
+ 
+load data local infile 'C:/Users/Eoin/Documents/GitHub/Data-Mining-CA/Web-Scrape/football_transfers/Extracted_csv_files/full_extract.csv' 
+INTO TABLE TransfersImport
+FIELDS 
+	TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES 
+	TERMINATED BY '\n'
+IGNORE 1 LINES
+(TransferType,Text,Year,Season,Player,RowId,Team,Owner,TransferTypeId);
+
+#SELECT * FROM transfersimport;
+
+
+
